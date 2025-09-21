d98e12a83f3ec4d2136891a0e0ded4050def381b
nphacks <164674563+nphacks@users.noreply.github.com>
2025-09-21 17:27:29 -0400

    Story notes routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Landing from "./Pages/Landing";
 import Screenplay from "./Pages/Screenplay";
 import Home from "./Pages/Home";
 import { Studio } from "./Pages/Studio";
-import { StoryNotes } from "./Pages/Subpages/Screenplay/StoryNotes";
+import StoryNotes from "./Pages/Subpages/Screenplay/StoryNotes";
 
 function App() {
   return (
@@ -12,8 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/screenplay/:screenplay_id?" element={<Screenplay />} />
-          <Route path="/screenplay/story_notes" element={<StoryNotes />} />
+          <Route path="/screenplay/:screenplay_id" element={<Screenplay />} />
+          <Route path="/screenplay/:screenplay_id/story_notes" element={<StoryNotes />} />
           <Route path="/studio" element={<Studio />} />
           
         </Routes>
